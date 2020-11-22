e802f7c3965e07eac648a5dd775fca73d88684f9
Lorand33 <73632937+Lorand33@users.noreply.github.com>
2020-11-22 18:06:25 +0200

    Four fragments added: MainScreenFragment, DetailFragment, ProfileFragment, SplashScreenFragment
    Splash screen's function added
    
    Splash screen's layout added
    
    Navigation graph(for four fragments) and bottom menu(Main menu, Profile) added

--- app/src/main/java/com/example/foodapplicationandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/foodapplicationandroidproject/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.foodapplicationandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodapplicationandroidproject.databinding.ActivityMainBinding
+import com.example.foodapplicationandroidproject.viewModels.ApiViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    var bottomNavigationView: BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        bottomNavigationView = binding.bottomNavMenu
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            bottomNavigationView!!,
+            navHostFragment!!.navController
+        )
+        bottomNavigationView!!.visibility = View.GONE
     }
 }
